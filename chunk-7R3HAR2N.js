9af474b9341a6e2e9fccbce1d77bce67852d6f6d
dxc888168 <138585557+dxc888168@users.noreply.github.com>
2025-06-07 07:12:02 +0000

    Deploying to gh-pages from @ dxc888168/nav@89a0dbc473965373fe988fd4e243930b7bc50f20 🚀

---
+++ chunk-7R3HAR2N.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-FGTALMYY.js";import{Fb as e,vb as r}from"./chunk-6G54DZ44.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
